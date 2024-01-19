aa3b68271234cc723cebbb0298ae9b9d8b67883b
PraVeen <kaushik281003@gmail.com>
2024-01-20 02:18:22 +0530

    Create,delete,update added

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from .models import Post
-
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -14,5 +14,24 @@ class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
     context_object_name='Detail_object'
+
+class BlogCreateView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields = ['title', 'author', 'body']
+    
+class BlogEditView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title','body']
+    
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')
+    
+    
+
+
 
 
